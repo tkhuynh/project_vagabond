ae4c4f7408d1ef180611c45dc6cec4232a4089f4
tkhuynh <thequest2311@yahoo.com>
2016-01-04 16:15:54 -0800

    add navbar and style a little bit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
   	@user = User.find_by_email(params[:email])
   	if @user && @user.authenticate(params[:password])
   		session[:user_id] = @user.id
-  		redirect to root_path
+  		redirect_to root_path
   	else
   		flash[:error] = "Incorrect email or password."
   		redirect_to login_path
