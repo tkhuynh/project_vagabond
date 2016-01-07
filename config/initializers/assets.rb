e7beae0cff2e65f17f8e72ad0b3061bf6368e9f9
abeytaadam <measalion@gmail.com>
2016-01-06 18:49:16 -0800

    City banners and styling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( pic1.png, pic2.png, ProjectVagabond.png, ProjectVagabondlogo-01.png, ProjectVagabondParis-02 )
+Rails.application.config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
